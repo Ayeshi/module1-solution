ef92a84967e8a4a416a69ba68f804df4c4f0ac9c
Ayeshi <ayeshi.coursera@gmail.com>
2016-12-11 22:49:03 +0530

    First web file

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+
+angular.module('module1', [])
+.controller('Module1Controller', Module1Controller);
+
+Module1Controller.$inject = ['$scope'];
+function Module1Controller($scope) {
+     $scope.lunchItemSet = "";
+     $scope.message = "";
+
+     var comma = ',';
+
+     $scope.checkItems = function () {
+          var numOfLunchItem = splitLunchItems($scope.lunchItemSet , comma);
+          if($scope.lunchItemSet == "" || $scope.lunchItemSet == " " ){
+               $scope.message = "Please enter data first!";
+          }else if (numOfLunchItem <= 3) {
+               $scope.message = "Enjoy!";
+          }else if (numOfLunchItem > 3) {
+               $scope.message = "Too much!";          
+          }else {
+               $scope.message = "Error"
+          }
+     }
+
+     function splitLunchItems(itemsToSplit,separator){
+
+          var arrayOfLunchItems = itemsToSplit.split(separator);
+
+          var totalLunchItems = arrayOfLunchItems.length;
+
+          return totalLunchItems;
+     }
+
+     //$scope.splitLunchItems($scope.lunchItemSet , comma);
+
+
+}
+
+})();
